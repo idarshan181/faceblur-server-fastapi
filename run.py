e3d7498c2d82638ee2ff6c14d1aa7377f979368d
Darshan Patel <work.idarshan18@gmail.com>
2024-11-09 21:26:48 -0330

    build: changed nginx and default running script

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import os
 
 if __name__ == "__main__":
-    os.system("poetry run uvicorn --host=0.0.0.0 app.main:app --reload")
-    # os.system("poetry run gunicorn app.main:app --workers 10 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 --reload")
+    os.system("poetry run uvicorn --host=127.0.0.1 --port=8000 app.main:app --reload")
+# --workers 3
+    # os.system("poetry run gunicorn app.main:app --workers 10 --worker-class uvicorn.workers.UvicornWorker --bind 127.0.0.1:8000 --reload")
